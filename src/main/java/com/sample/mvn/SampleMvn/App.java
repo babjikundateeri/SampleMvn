65df93073465c1f5bde595b49f84726d3555b78d
babjikundateeri <babji.kundateeri@gmail.com>
2016-04-05 11:21:41 +0530

    added resources

--- src/main/java/com/sample/mvn/SampleMvn/App.java
+++ src/main/java/com/sample/mvn/SampleMvn/App.java
@@ -1,13 +1,20 @@
 package com.sample.mvn.SampleMvn;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static Logger LOGGER = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
+    	LOGGER.info("Info ");
+    	LOGGER.debug("debug ");
+    	LOGGER.warn("warn");
+    	LOGGER.error("ERROR");
         System.out.println( "Hello World!" );
     }
 }
